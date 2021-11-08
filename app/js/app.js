39aa89b0eadebb1a0fde148fef1101f5fea487ca
Edmond-999 <82581663+Edmond-999@users.noreply.github.com>
2021-11-08 14:08:59 +0400

    .

--- app/js/app.js
+++ app/js/app.js
@@ -1,1 +1,24 @@
-console.log("Hello wolrd");
+$(document).ready(function() {
+
+  $(window).scroll(function() {
+    // Show service slider black content
+    if (($(this).scrollTop()) > 10) {
+      $('.header').addClass('with-bg')
+    } else {
+      $('.header').removeClass('with-bg')
+    }
+  });
+
+
+  // $('.burger').click(function(){
+  //   if($('.mobile-menu-opened').hasClass('opened')) {
+  //     $('.mobile-menu').removeClass('opened')
+  //   } else {
+  //     $('.mobile').addClass('opened')
+  //   }
+  // })  
+
+  // $('.close').click(function(){
+  //   $('.mobile-menu').removeClass('opened')
+  // })
+});
