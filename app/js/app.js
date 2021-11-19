5cba2ec8a1fcfcad9641e52dee46452cb01a1a36
Hovhannes <hovhannes.g@sdc.am>
2021-11-19 12:20:37 +0400

    fix pricing cards changing

--- app/js/app.js
+++ app/js/app.js
@@ -1,38 +1,44 @@
-$(document).ready(function() {
-
-  $(window).scroll(function() {
+$(document).ready(function () {
+  $(window).scroll(function () {
     // Show service slider black content
-    if (($(this).scrollTop()) > 10) {
-      $('.header').addClass('with-bg')
+    if ($(this).scrollTop() > 10) {
+      $(".header").addClass("with-bg");
     } else {
-      $('.header').removeClass('with-bg')
+      $(".header").removeClass("with-bg");
     }
   });
 
   if ($(window).scrollTop() > 1) {
-    $('.header').addClass('with-bg')
+    $(".header").addClass("with-bg");
   } else {
-    $('.header').removeClass('with-bg')
+    $(".header").removeClass("with-bg");
   }
 
-  $('.switcher').click(function(){
-    if ($(this).hasClass('active')) {
-      $(this).removeClass('active')
+  $(".switcher").click(function () {
+    if ($(this).hasClass("active")) {
+      $(this).removeClass("active");
     } else {
-      $(this).addClass('active')
+      $(this).addClass("active");
     }
-  })
+
+    if ($(".monthly-price").hasClass("hide")) {
+      $(".monthly-price").removeClass("hide");
+      $(".annualy-price").addClass("hide");
+    } else {
+      $(".monthly-price").addClass("hide");
+      $(".annualy-price").removeClass("hide");
+    }
+  });
 
   // Open mobile menu
-  $('.burger').click(function(){
-    $('.mobile-menu').addClass('opened')
-    $('body').addClass('scroll-off')
-  })  
+  $(".burger").click(function () {
+    $(".mobile-menu").addClass("opened");
+    $("body").addClass("scroll-off");
+  });
 
   // Close mobile menu
-  $('.close-btn').click(function(){
-    $('.mobile-menu').removeClass('opened')
-    $('body').removeClass('scroll-off')
-  })
-  
+  $(".close-btn").click(function () {
+    $(".mobile-menu").removeClass("opened");
+    $("body").removeClass("scroll-off");
+  });
 });
